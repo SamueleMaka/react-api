c27c4b78c95a5fa72a44239071e76bcf7587ae43
SamueleMaka <samuele.maka.imt@gmail.com>
2025-07-29 20:56:42 +0200

    feat: creazione proggetto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import SingleCard from './components/SingleCard'
+function App() {
+
+  return (
+    <></>
+  )
+}
+
+export default App
