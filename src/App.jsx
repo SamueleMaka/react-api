ca2fde9b3b4d86f860679409b915a9ca978689f9
SamueleMaka <samuele.maka.imt@gmail.com>
2025-07-29 21:04:19 +0200

    feat: chiamata API

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import './App.css'
 import axios from 'axios'
 import SingleCard from './components/SingleCard'
 function App() {
+  const URL_API = "https://lanciweb.github.io/demo/api/actors/"
+  const [actors, setActors] = useState([])
 
+  useEffect(() => {
+    axios.get(URL_API).then(res => {
+        const actorData = res.data
+        setActors(actorData)
+    })
+  }, [])
+  
   return (
     <></>
   )
